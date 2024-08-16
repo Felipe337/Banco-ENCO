1821ee6ec490ea010c6904d4b957eccf7f23c0d5
Felipe337 <116205272+Felipe337@users.noreply.github.com>
2024-08-16 09:24:56 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+//DESENVOLVIDO POR FELIPE A. ARAÚJO & ROBERTA R. F. ASENCIO
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "app.h"
+
+    int main(){
+    menu();
+    return 0;
+}
